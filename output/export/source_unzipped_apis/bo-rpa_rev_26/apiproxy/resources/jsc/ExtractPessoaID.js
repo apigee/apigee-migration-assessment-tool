6c39a0c98227f57accdb3aa1734ecf017665898b
jeof <jeof@jeof.net>
2025-06-26 16:14:27 -0600

    Deleting accidental commit of output

--- output/export/source_unzipped_apis/bo-rpa_rev_26/apiproxy/resources/jsc/ExtractPessoaID.js
+++
@@ -1,4 +0,0 @@
- var json = JSON.parse(context.getVariable("coreBankingResponse.content"))
- var id_pessoa = json.core_banking.idPessoa
- 
- context.setVariable("id_pessoa", id_pessoa);
--- output/export/source_unzipped_apis/pagos-fisicos-autoservicio-creditos-abonos-v1/apiproxy/resources/jsc/JS-ContextFlow.js
+++
@@ -1,3 +0,0 @@
-print('JS-ContextFlow [INFO]');
-context.setVariable('logging.contex.flow', context.flow);
-context.setVariable('logging.error.state', context.getVariable('error.state'));
